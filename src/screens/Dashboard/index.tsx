53f98905288afb6220ba01cb1cdd9f00e4541a00
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-09-11 12:04:22 -0300

    :man: Adding header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import { 
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+} from "./styles";
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: "https://github.com/CauanDZN.png" }} />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Cauan</UserName>
+                        </User>
+                    </UserInfo> 
+                </UserWrapper>
+                
+
+            </Header>
+
         </Container>
     )
 }
