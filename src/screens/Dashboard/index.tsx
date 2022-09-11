14da1f2fbdf29844148a2ee84695a4295ebaa4c7
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-09-11 15:50:07 -0300

    feat: cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -36,9 +36,9 @@ export function Dashboard(){
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={{ paddingHorizontal: 24 }}
             >
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
+                <HighlightCard title="Entradas" type="up" amount="R$ 2.000,00" lastTransaction="3 de Outubro de 2022"/>
+                <HighlightCard title="Saídas" type="down" amount="R$ 100,00" lastTransaction="3 de Outubro de 2022" />
+                <HighlightCard title="Total" type="total" amount="R$ 1.900,00" lastTransaction="3 de Outubro de 2022" />
             </HighlightCards>
             
         </Container>
