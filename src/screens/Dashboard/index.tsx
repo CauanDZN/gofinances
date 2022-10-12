6b06beb8e80b0bb3baec3e8da72fd1448ab3a1d4
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-10-12 11:02:19 -0300

    feat: transaction cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard } from "../../components/TransactionCard";
 
-import { 
+import {
     Container,
     Header,
     UserWrapper,
@@ -11,11 +12,23 @@ import {
     UserGreeting,
     UserName,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title
 } from "./styles";
 
-export function Dashboard(){
-    return(
+export function Dashboard() {
+    const data = {
+        title: "Desenvolvimento de site",
+        amount: "R$30.000.00",
+        category: {
+            name: "Vendas",
+            icon: "dollar-sign"
+        },
+        date: "03/10/2022",
+    }
+
+    return (
         <Container>
             <Header>
                 <UserWrapper>
@@ -25,8 +38,8 @@ export function Dashboard(){
                             <UserGreeting>Olá, </UserGreeting>
                             <UserName>Cauan</UserName>
                         </User>
-                    </UserInfo> 
-                    
+                    </UserInfo>
+
                     <Icon name="power" />
                 </UserWrapper>
             </Header>
@@ -36,11 +49,19 @@ export function Dashboard(){
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={{ paddingHorizontal: 24 }}
             >
-                <HighlightCard title="Entradas" type="up" amount="R$ 2.000,00" lastTransaction="3 de Outubro de 2022"/>
+                <HighlightCard title="Entradas" type="up" amount="R$ 2.000,00" lastTransaction="3 de Outubro de 2022" />
                 <HighlightCard title="Saídas" type="down" amount="R$ 100,00" lastTransaction="3 de Outubro de 2022" />
                 <HighlightCard title="Total" type="total" amount="R$ 1.900,00" lastTransaction="3 de Outubro de 2022" />
             </HighlightCards>
-            
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionCard
+                    data={data}
+                />
+            </Transactions>
+
         </Container>
     )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,7 +4,7 @@ import { Feather } from "@expo/vector-icons"
 
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-import { 
+import {
     RFPercentage,
     RFValue
 } from 'react-native-responsive-fontsize';
@@ -13,14 +13,14 @@ import theme from "../../global/styles/theme";
 
 export const Container = styled.View`
     flex: 1;
-    background-color: ${({theme}) => theme.colors.background};
+    background-color: ${({ theme }) => theme.colors.background};
 `
 
 export const Header = styled.View`
     width: 100%;
     height: ${RFPercentage(42)}px;
 
-    background-color: ${({theme}) => theme.colors.primary};
+    background-color: ${({ theme }) => theme.colors.primary};
 
     justify-content: center;
     align-items: flex-start;
@@ -48,28 +48,28 @@ export const Photo = styled.Image`
     height: ${RFValue(48)}px;
 
     border-radius: 10px;
-`; 
+`;
 
 export const User = styled.View`
     margin-left: 17px;
-`; 
+`;
 
 export const UserGreeting = styled.Text`
-    color: ${({theme}) => theme.colors.shape};
+    color: ${({ theme }) => theme.colors.shape};
 
     font-size: ${RFValue(18)}px;
-    font-family: ${({theme}) => theme.fonts.regular};
-`; 
+    font-family: ${({ theme }) => theme.fonts.regular};
+`;
 
 export const UserName = styled.Text`
-    color: ${({theme}) => theme.colors.shape};
+    color: ${({ theme }) => theme.colors.shape};
 
     font-size: ${RFValue(18)}px;
-    font-family: ${({theme}) => theme.fonts.bold};
-`; 
+    font-family: ${({ theme }) => theme.fonts.bold};
+`;
 
 export const Icon = styled(Feather)`
-    color: ${({theme}) => theme.colors.secondary};
+    color: ${({ theme }) => theme.colors.secondary};
 
     font-size: ${RFValue(24)}px;
 `;
@@ -77,10 +77,24 @@ export const Icon = styled(Feather)`
 export const HighlightCards = styled.ScrollView.attrs({
     horizontal: true,
     showsHorizontalScrollIndicator: false,
-    contentContainerStyle: {paddingHorizontal: 24},
+    contentContainerStyle: { paddingHorizontal: 24 },
 })`
     width: 100%;
     position: absolute;
 
     margin-top: ${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+    flex: 1%;
+    padding: 0 24px;
+
+    margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+
+    margin-bottom: 16px;
 `;
