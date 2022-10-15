b129308a8597d5c661e499ad8fc364bf0d97d9a0
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-10-15 13:32:29 -0300

    feat: AsyncStorage

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import {
     Modal,
@@ -10,9 +10,10 @@ import {
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
 
-import { FieldValues, useForm } from 'react-hook-form';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import { Input } from "../../components/Form/Input";
+import { useForm } from 'react-hook-form';
+
 import { InputForm } from "../../components/Form/InputForm";
 
 import { Button } from "../../components/Form/Button";
@@ -50,6 +51,8 @@ export function Register() {
     const [transactionType, setTransactionType] = useState("")
     const [categoryModalOpen, setCategoryModalOpen] = useState(false)
 
+    const dataKey = "@gofinances:transactions"
+
     const [category, setCategory] = useState({
         key: 'category',
         name: 'Categoria',
@@ -75,7 +78,7 @@ export function Register() {
         setCategoryModalOpen(false);
     }
 
-    function handleRegister(form: Partial<FormData>) {
+    async function handleRegister(form: Partial<FormData>) {
         if (!transactionType)
             return Alert.alert('Selecione o tipo de transação!')
 
@@ -89,8 +92,23 @@ export function Register() {
             category: category.key
         }
 
-        console.log(data)
+        try {
+            await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Não foi possível salvar.")
+        }
     }
+
+    useEffect(() => {
+        async function loadData() {
+            const data = await AsyncStorage.getItem(dataKey)
+            console.log(JSON.parse(data!))
+        }
+
+        loadData()
+    }, [])
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
